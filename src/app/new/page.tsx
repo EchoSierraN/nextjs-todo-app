71fd02b61dbdd5dec02d2a8e253db0ede1dcc665
Elias Shemsu <echosierran@gmail.com>
2024-02-09 11:22:07 +0300

    Cancel button

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>This is the new page.</h1>;
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">NEW</h1>
+      </header>
+      <form className="flex flex-col gap-2">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        ></input>
+        <div className="flex gap-1 justify-end">
+          <Link
+            href={".."}
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded outline-none hover:bg-slate-700 focus-within:bg-slate-700"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py-1 rounded outline-none hover:bg-slate-700 focus-within:bg-slate-700"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
