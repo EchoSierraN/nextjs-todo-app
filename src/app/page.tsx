c27b551617c9a2a3580410a55c2699f710c1de0b
Elias Shemsu <echosierran@gmail.com>
2024-02-09 10:51:06 +0300

    Reading todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
+import prisma from "@/db";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+  // await prisma.todo.create({ data: { title: "Todo 1", complete: false } });
+
   return (
     <>
-      <header>
-        <h1>The Emperor protects.</h1>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">TODO</h1>
+        <Link
+          href={"/new"}
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded outline-none hover:bg-slate-700 focus-within:bg-slate-700"
+        >
+          NEW
+        </Link>
       </header>
-      <Link href={"/new"}>Click new</Link>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
